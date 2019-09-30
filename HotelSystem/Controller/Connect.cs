7083672b2557aaafe85790c7b8cb2d92c072763e
diego <draguxen26@gmail.com>
2019-09-30 15:56:26 +0200

    Added Connection methods
    Open & close connection

--- HotelSystem/Controller/Connect.cs
+++ HotelSystem/Controller/Connect.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 
 namespace HotelSystem.Controller {
     class Connect {
-        private MySqlConnection connection = new MySqlConnection("");
+        public MySqlConnection Connection { get; private set; }
+
+        public Connect() {
+            Connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=hotel_db;");
+        }
+
+        public void OpenConnection() {
+            if (Connection.State == ConnectionState.Closed) {
+                Connection.Open();
+            }
+        }
+
+        public void CloseConnection() {
+            if (Connection.State == ConnectionState.Open) {
+                Connection.Close();
+            }
+        }
     }
 }
